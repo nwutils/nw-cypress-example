88790865bf49b6ccec76b3d0706770261fe9f0de
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2021-07-12 15:53:08 -0400

    browser plugin

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -1,2 +1,18 @@
+const execa = require('execa');
+const path = require('path');
+
 module.exports = function (on, config) {
+  const browserPath = path.join(__dirname, '..', '..', 'node_modules', 'nw', 'nwjs', 'nw.exe');
+  const browser = {
+    name: 'NW.js',
+    channel: 'stable',
+    family: 'chromium',
+    displayName: 'NW.js',
+    version: '0.54.1',
+    path: browserPath,
+    majorVersion: 54
+  };
+  return {
+    browsers: config.browsers.concat(browser)
+  };
 };
